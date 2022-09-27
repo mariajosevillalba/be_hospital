6813107665c2939706aefea5b21a619f371c56b9
unsitio1234-admin <majosevillalba@live.com>
2022-09-26 22:33:47 -0500

    urls, views, serializer

--- hospitalBackend/models/paciente.py
+++ hospitalBackend/models/paciente.py
@@ -1,17 +1,12 @@
 from django.db import models
 from .usuario import Usuario
 from .medico import Medico
-from .historia import Historia
-from .enfermero import Enfermero
-from .familiar import Familiar
+
 
 class Paciente(models.Model):
     id=models.AutoField(primary_key=True)
-    usuario=models.ForeignKey(Usuario, related_name= 'paciente', on_delete=models.CASCADE)
-    familiar=models.ForeignKey(Familiar, related_name= 'paciente', on_delete=models.CASCADE)
-    medico=models.ForeignKey(Medico, related_name= 'paciente', on_delete=models.CASCADE)
-    enfermero=models.ForeignKey(Enfermero, related_name= 'paciente', on_delete=models.CASCADE)
-    historia=models.ForeignKey(Historia, related_name= 'paciente', on_delete=models.CASCADE)
+    usuario=models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    medico=models.ForeignKey(Medico, on_delete=models.CASCADE)
 
     
    
